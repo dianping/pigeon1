83c3ed170f7121a2861672b53ecda156af433ba7
sean.wang <xalinx@gmail.com>
2012-07-26 14:49:31 +0800

    delete dpsf-example subproj

--- dpsf-example/src/main/java/com/dianping/dpsf/test/TestException.java
+++
@@ -1,9 +0,0 @@
-package com.dianping.dpsf.test;
-
-public class TestException extends Exception{
-	
-	public TestException(String msg){
-		super(msg);
-	}
-
-}
--- dpsf-example/src/main/java/com/dianping/dpsf/test/TestIFace.java
+++
@@ -1,7 +0,0 @@
-package com.dianping.dpsf.test;
-
-public interface TestIFace {
-	
-	public int test(int arg) throws TestException;
-
-}
